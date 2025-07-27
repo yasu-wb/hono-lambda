2d714f0760df4cd4769555e6d02cb7212bfa1639
yasu-wb <yshr.kihara@gmail.com>
2025-07-27 17:09:27 +0900

    Add Sample Post Code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/aws-lambda'
+import type { LambdaEvent, LambdaContext } from 'hono/aws-lambda'
 
-const app = new Hono()
+type Bindings = {
+  event: LambdaEvent
+  context: LambdaContext
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+app.post('/hono-lambda', async (c) => {
+  const request = await c.req.json()
+
+  return c.json({
+    message: 'Hello from Hono Lambda!',
+    request: request,
+  })
 })
 
 export const handler = handle(app)
